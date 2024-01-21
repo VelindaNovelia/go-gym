993a9367170ae24dfc2947cd0b28ef12900ec88f
Velinda Novelia Candra <2173004@maranatha.ac.id>
2024-01-21 17:05:35 +0700

    revisi

--- db/gym.sql
+++ db/gym.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Jan 2024 pada 16.09
--- Versi server: 10.4.28-MariaDB
--- Versi PHP: 8.2.4
+-- Waktu pembuatan: 21 Jan 2024 pada 11.04
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,110 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `daftar`
+--
+
+CREATE TABLE `daftar` (
+  `iddaftar` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `role` varchar(255) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `daftar`
+--
+
+INSERT INTO `daftar` (`iddaftar`, `name`, `email`, `password`, `role`) VALUES
+(1, 'Yemmi', '2173012@marnat', '123', 'user'),
+(2, 'Velinda Novelia Candra', '2173004@maranatha.ac.id', '123', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `keluar`
+--
+
+CREATE TABLE `keluar` (
+  `idkeluar` int(11) NOT NULL,
+  `idbarang` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `pembeli` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `masuk`
+--
+
+CREATE TABLE `masuk` (
+  `idmasuk` int(11) NOT NULL,
+  `idbarang` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `ket` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `masuk`
+--
+
+INSERT INTO `masuk` (`idmasuk`, `idbarang`, `tanggal`, `ket`, `qty`) VALUES
+(6, 3, '2024-01-17 15:05:37', 'Velin', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `member`
+--
+
+CREATE TABLE `member` (
+  `idmember` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `tipe` varchar(25) NOT NULL,
+  `trainer` varchar(25) NOT NULL,
+  `tglmul` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `pembayaran` varchar(25) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `member`
+--
+
+INSERT INTO `member` (`idmember`, `name`, `tipe`, `trainer`, `tglmul`, `pembayaran`, `total`) VALUES
+(1, 'Velinda Novelia Candra', '1 Month', '-', '2024-01-17 13:56:57', 'virtual_account', 250000),
+(2, 'Velinda Novelia Candra', '1 Month', 'Yoga Trainer', '2024-01-17 14:31:30', 'virtual_account', 1250000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `merch`
+--
+
+CREATE TABLE `merch` (
+  `idmerch` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `alamat` varchar(25) NOT NULL,
+  `desk` varchar(25) NOT NULL,
+  `payment` varchar(25) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `merch`
+--
+
+INSERT INTO `merch` (`idmerch`, `nama`, `alamat`, `desk`, `payment`, `total`) VALUES
+(1, 'Yemmi', 'das', 'T-Shirt Special', 'virtual_account', 100000),
+(2, 'Velinda Novelia Candra', 'Jl. Gempol Elok II no. 41', 'T-Shirt Special', 'virtual_account', 100000);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `stock`
 --
 
@@ -33,7 +137,7 @@ CREATE TABLE `stock` (
   `stok` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   `vendor` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `stock`
@@ -42,12 +146,42 @@ CREATE TABLE `stock` (
 INSERT INTO `stock` (`idbarang`, `namabarang`, `stok`, `harga`, `vendor`) VALUES
 (1, 'T-Shirt', 200, 100000, 'Toko Raymond'),
 (2, 'Hoodie ', 50, 200000, 'Toko Semafit'),
-(3, 'Tumbler Spesial', 200, 125000, 'Marnat Store'),
+(3, 'Tumbler Spesial', 250, 125000, 'Marnat Store'),
 (4, 'Tote Bag', 300, 125000, 'Warung Staff');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `daftar`
+--
+ALTER TABLE `daftar`
+  ADD PRIMARY KEY (`iddaftar`);
+
+--
+-- Indeks untuk tabel `keluar`
+--
+ALTER TABLE `keluar`
+  ADD PRIMARY KEY (`idkeluar`);
+
+--
+-- Indeks untuk tabel `masuk`
+--
+ALTER TABLE `masuk`
+  ADD PRIMARY KEY (`idmasuk`);
+
+--
+-- Indeks untuk tabel `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`idmember`);
+
+--
+-- Indeks untuk tabel `merch`
+--
+ALTER TABLE `merch`
+  ADD PRIMARY KEY (`idmerch`);
 
 --
 -- Indeks untuk tabel `stock`
@@ -60,10 +194,40 @@ ALTER TABLE `stock`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `daftar`
+--
+ALTER TABLE `daftar`
+  MODIFY `iddaftar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `keluar`
+--
+ALTER TABLE `keluar`
+  MODIFY `idkeluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `masuk`
+--
+ALTER TABLE `masuk`
+  MODIFY `idmasuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `member`
+--
+ALTER TABLE `member`
+  MODIFY `idmember` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `merch`
+--
+ALTER TABLE `merch`
+  MODIFY `idmerch` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `stock`
 --
 ALTER TABLE `stock`
-  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
